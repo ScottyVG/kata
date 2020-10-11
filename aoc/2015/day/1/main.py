6b75421320c945690473800152c0588efcabc28f
scottyvg <svangilder@gmail.com>
2020-10-11 10:56:09 -0500

    adds 2015 aoc

---
+++ aoc/2015/day/1/main.py
@@ -0,0 +1,25 @@
+def main():
+    with open("input.txt", "r") as input_file:
+        directions = input_file.read()
+
+    floor = 0
+    
+    def Convert(string): # Convert string to a list by every char
+        list1=[] 
+        list1[:0]=string 
+        return list1 
+
+    dlist = Convert(directions)
+    print(len(dlist)) 
+
+    for i in range(0, len(dlist)):
+        if dlist[i] == "(":
+            floor += 1
+        elif dlist[i] == ")":
+            floor -= 1
+    
+    print(floor)
+
+if __name__ == "__main__":
+    main()
+
